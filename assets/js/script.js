c5d84c554e10b94761c1259fa4e90d7b388220eb
Hernán Alvarado <hernnalvarado@gmail.com>
2023-08-17 13:30:48 -0400

    primer commit css y js terminado

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+function obtenerCodigo() {
+    const checkbox = document.getElementById("check");
+    const codigo = document.getElementById("codigo");
+    if (!checkbox.checked) {
+      alert("Para obtener el código de descuento, debe aceptar los términos y condiciones");
+    } else {
+      codigo.classList.remove("invisible");
+    }
+  }
+
+
+  let idCodigo = document.getElementById('codigo');
+  idCodigo.innerHTML = 'RXTY60';
