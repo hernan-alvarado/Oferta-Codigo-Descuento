a62d7a56836d176d5134efe16bed820f6ed52fa9
Hernán Alvarado <hernnalvarado@gmail.com>
2023-08-17 13:43:27 -0400

    commit dos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-
 function obtenerCodigo() {
-    const checkbox = document.getElementById("check");
-    const codigo = document.getElementById("codigo");
-    if (!checkbox.checked) {
-      alert("Para obtener el código de descuento, debe aceptar los términos y condiciones");
-    } else {
-      codigo.classList.remove("invisible");
-    }
-  }
+	const checkbox = document.getElementById("check");
+	const codigo = document.getElementById("codigo");
+	if (!checkbox.checked) {
+		alert(
+			"Para obtener el código de descuento, debe aceptar los términos y condiciones"
+		);
+	} else {
+		codigo.classList.remove("invisible");
+	}
+}
 
-
-  let idCodigo = document.getElementById('codigo');
-  idCodigo.innerHTML = 'RXTY60';
+let idCodigo = document.getElementById("codigo");
+idCodigo.innerHTML = "RXTY60";
